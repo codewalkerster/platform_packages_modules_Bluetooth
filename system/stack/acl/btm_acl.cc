notmikeb/aosp15_fluoride

Encrypt LE link immediately on reconnection
LE link must be encrypted immediately on connection if device are
already bonded.

This is a backport of ag/29056565, but the code needs to go in a
different location because that patch relies on recent feature work.

Ignore-AOSP-First: security
Test: mmm packages/modules/Bluetooth
Bug: 288144143
Merged-In: I607af8214f4fd9c3905f2e053ba136cd6189361f
Merged-In: I8bd0ef5ae1750096fe72feb334022b9895f3b26e
Change-Id: I7147c837ecab6c67943fc6fd78a9949f3381df62

system/stack/acl/btm_acl.cc
@@ -1079,6 +1079,17 @@ void StackAclBtmAcl::btm_establish_continue(tACL_CONN* p_acl) {
                 PRIVATE_ADDRESS(p_acl->RemoteAddress()));
     }
     btm_set_link_policy(p_acl, btm_cb.acl_cb_.DefaultLinkPolicy());
+  } else if (p_acl->is_transport_ble()) {
+    tBTM_SEC_DEV_REC* p_dev_rec = btm_find_dev(p_acl->remote_addr);
+
+    if (p_dev_rec == nullptr) {
+      LOG_WARN("No security record for %s",
+               PRIVATE_ADDRESS(p_acl->RemoteAddress()));
+    } else if (p_dev_rec->is_le_link_key_known()) {
+      btm_ble_set_encryption(
+          p_acl->remote_addr, BTM_BLE_SEC_ENCRYPT,
+          p_dev_rec->role_central ? HCI_ROLE_CENTRAL : HCI_ROLE_PERIPHERAL);
+    }
   }
   NotifyAclLinkUp(*p_acl);
 }

codewalkerster/platform_packages_modules_Bluetooth

(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:0064c0b351fc0902ebbf7bff2e84ce888abb396e)
Merged-In: I7147c837ecab6c67943fc6fd78a9949f3381df62

system/stack/acl/btm_acl.cc
@@ -1140,6 +1140,17 @@ void StackAclBtmAcl::btm_establish_continue(tACL_CONN* p_acl) {
                 ADDRESS_TO_LOGGABLE_CSTR(p_acl->RemoteAddress()));
     }
     btm_set_link_policy(p_acl, btm_cb.acl_cb_.DefaultLinkPolicy());
+  } else if (p_acl->is_transport_ble()) {
+    tBTM_SEC_DEV_REC* p_dev_rec = btm_find_dev(p_acl->remote_addr);
+
+    if (p_dev_rec == nullptr) {
+      LOG_WARN("No security record for %s",
+               ADDRESS_TO_LOGGABLE_CSTR(p_acl->RemoteAddress()));
+    } else if (p_dev_rec->is_le_link_key_known()) {
+      btm_ble_set_encryption(
+          p_acl->remote_addr, BTM_BLE_SEC_ENCRYPT,
+          p_dev_rec->role_central ? HCI_ROLE_CENTRAL : HCI_ROLE_PERIPHERAL);
+    }
   }
   NotifyAclLinkUp(*p_acl);
 }